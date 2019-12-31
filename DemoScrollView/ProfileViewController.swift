8ba23d9d88b29d8c8901413f544f0ef16dcf617d
DucTV <space.ranger9x@gmail.com>
2019-12-31 14:09:30 +0700

    btvn

---
+++ DemoScrollView/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  DemoScrollView
+//
+//  Created by Tran Viet Duc on 12/31/19.
+//  Copyright © 2019 Taof. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var imageAvata: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageAvata.layer.cornerRadius = 100
+    }
+    
+
+    
+
+}
